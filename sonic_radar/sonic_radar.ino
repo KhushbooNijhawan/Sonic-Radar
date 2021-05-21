7f6abd62ab7d83c2a800b8c5dc4c4427e3f2365a
KhushbooNijhawan <79616524+KhushbooNijhawan@users.noreply.github.com>
2021-05-21 09:19:40 +0530

    Update sonic_radar.ino

--- sonic_radar/sonic_radar.ino
+++ sonic_radar/sonic_radar.ino
@@ -1,56 +1,44 @@
-#include <Servo.h>
-
-const int trigPin = 8;
-const int echoPin = 10;
-
+#include <Servo.h>. 
+const int trigPin = 10;
+const int echoPin = 11;
 long duration;
-int distinCM;
-
+int distance;
 Servo myServo;
-
-void setup() 
-{
+void setup() {
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
   Serial.begin(9600);
-  myServo.attach(11);
+  myServo.attach(12);
 }
-void loop() 
-{
-  for(int i=0;i<=180;i++)
-  {
-    myServo.write(i);
-    delay(50);
-    
-    digitalWrite(trigPin, LOW); 
-    delayMicroseconds(2);
-    digitalWrite(trigPin, HIGH); 
-    delayMicroseconds(10);
-    digitalWrite(trigPin, LOW);
-    duration = pulseIn(echoPin, HIGH);
-    distinCM = duration*0.034/2;
-    
-    Serial.print(i);
-    Serial.print("*");
-    Serial.print(distinCM);
-    Serial.print("#");
-  }
+void loop() {
+  for(int i=15;i<=165;i++){  
+  myServo.write(i);
+  delay(30);
+  distance = calculateDistance();
   
-  for(int i=180;i>=0;i--)
-  {
-    myServo.write(i);
-    delay(50);
-    digitalWrite(trigPin, LOW); 
-    delayMicroseconds(2);
-    digitalWrite(trigPin, HIGH); 
-    delayMicroseconds(10);
-    digitalWrite(trigPin, LOW);
-    duration = pulseIn(echoPin, HIGH);
-    distinCM = duration*0.034/2;
-    
-    Serial.print(i);
-    Serial.print("*");
-    Serial.print(distinCM);
-    Serial.print("#");
+  Serial.print(i); 
+  Serial.print(",");
+  Serial.print(distance);
+  Serial.print("."); 
+  }
+  for(int i=165;i>15;i--){  
+  myServo.write(i);
+  delay(30);
+  distance = calculateDistance();
+  Serial.print(i);
+  Serial.print(",");
+  Serial.print(distance);
+  Serial.print(".");
   }
 }
+int calculateDistance(){ 
+  digitalWrite(trigPin, LOW); 
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH); 
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  distance= duration*0.034/2;
+  return distance;
+}
+
